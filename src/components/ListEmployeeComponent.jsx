815432d507a0e7756d7d28939625fb77e3e95b0c
tmadhuker <tmadhuker@gmail.com>
2020-07-28 22:55:31 +0530

    8 video

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ListEmployeeComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      employees: [],
+    };
+  }
+
+  
+
+export default ListEmployeeComponent;
