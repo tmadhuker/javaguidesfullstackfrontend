9a662d8149d2fbe710dab9ca5c8f3c661cdc310d
tmadhuker <tmadhuker@gmail.com>
2020-07-28 23:51:31 +0530

    11 video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={ListEmployeeComponent}></Route>
+            <Route path="/employees" component={ListEmployeeComponent}></Route>
+            {/* <Route path="/" component={ListEmployeeComponent}></Route> */}
+            <ListEmployeeComponent />
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
